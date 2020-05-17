ef59fa02773b1affc36163241f20fcf00cd9bf8e
Carlgo11 <Carlgo11@users.noreply.github.com>
2020-05-17 20:02:24 +0200

    Upload powershell script

---
+++ dark_theme.ps1
@@ -0,0 +1,11 @@
+﻿$light_time = 08
+$dark_time = 22
+[int]$time = Get-Date -Format HH
+if (($time -ge $dark_time) -OR ($time -le $light_time)){
+echo "dark mode"
+New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
+}else{
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 1
+echo "light mode"
+}
+exit
